fa2571ce2b70dda4fb36a24a0a27926cdb03f6e0
KombangkoeDias <kbdirifilation@gmail.com>
2023-02-03 02:09:10 +0800

    finish the frontend task

--- src/problem2/fancyform/src/components/fancy_form.js
+++ src/problem2/fancyform/src/components/fancy_form.js
@@ -10,16 +10,19 @@ import {
   Alert,
 } from "reactstrap";
 import "./fancy_form.css";
+import Spinner from "react-spinner-material";
 
 function FancyForm() {
   const [ethAddress, setEthAddress] = useState("");
   const [amount, setAmount] = useState("");
   const [otp, setOtp] = useState("");
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setSuccess("");
 
     if (!ethAddress || !amount || !otp) {
       setError("Please fill in all fields.");
@@ -45,11 +48,18 @@ function FancyForm() {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
+      // send the token
+      setSuccess(
+        `Successfully sent the token amount: ${amount} to address ${ethAddress}`
+      );
+      setEthAddress("");
+      setAmount("");
+      setOtp("");
     }, Math.random() * 3000);
   };
 
   return (
-    <div className="super-center">
+    <div className="super-center full-height">
       <Form onSubmit={handleSubmit} className="fancy-form">
         <Label>
           <h1>Send Token</h1>
@@ -85,10 +95,16 @@ function FancyForm() {
           />
         </FormGroup>
         {error && (
-          <Alert color="danger" className="fancy-form__error">
+          <Alert color="danger" className="fancy-form__error small-height">
             {error}
           </Alert>
         )}
+        {success && (
+          <Alert color="success" className="fancy-form__success small-height">
+            {success}
+          </Alert>
+        )}
+        {!success && !error && <div className="small-height"></div>}
         <Row>
           <Col xs="6" className="fancy-form__button_col">
             <Button
@@ -106,6 +122,14 @@ function FancyForm() {
             <Button className="fancy-form__submit__button">SEND TOKENS</Button>
           </Col>
         </Row>
+        {loading && (
+          <Row className="super-center original-height fancy-form__spinner_row">
+            <Spinner />
+          </Row>
+        )}
+        {!loading && (
+          <div className="original-height fancy-form__spinner_row"></div>
+        )}
       </Form>
     </div>
   );
